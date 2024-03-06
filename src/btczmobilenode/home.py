d01f23df97c44262da596d204bd3066124fd682b
EzzyG <59780480+ezzygarmyz@users.noreply.github.com>
2024-03-06 21:49:52 +0100

    Update home.py

--- src/btczmobilenode/home.py
+++ src/btczmobilenode/home.py
@@ -1,10 +1,8 @@
 import toga
 from toga.colors import rgb
-from toga.style.pack import Pack, COLUMN
-import requests
-import json
-import os
+from toga.style.pack import Pack, COLUMN, ROW
 
+from btczmobilenode.blockchaininfo import get_total_balance, get_blockchain_info
 
 class MainBalanceBox(toga.Box):
     def __init__(self, *args, **kwargs):
@@ -13,8 +11,9 @@ class MainBalanceBox(toga.Box):
     
     def set_app(self, app):
         self.app = app
+        config_path = self.app.paths.config / 'config.json'
 
-        total, transparent, private = self.get_total_balance()
+        total, transparent, private = get_total_balance(config_path)
         
         self.balance_box = toga.Box(
             children=[
@@ -44,19 +43,23 @@ class MainBalanceBox(toga.Box):
                 
                 toga.Label(
                     f"T : {transparent} BTCZ",
-                    style=Pack(text_align="center",
-                    font_size="10",
-                    font_family="monospace",
-                    color=rgb(240, 248, 255),
-                    padding=(15, 0, 0, 0))),
+                    style=Pack(
+                        text_align="center",
+                        font_size="10",
+                        font_family="monospace",
+                        font_weight="bold",
+                        color=rgb(240, 248, 255),
+                        padding=(15, 0, 0, 0))),
                 
                 toga.Label(
                     f"Z : {private} BTCZ",
-                    style=Pack(text_align="center",
-                    font_size="10",
-                    font_family="monospace",
-                    color=rgb(0, 179, 241),
-                    padding=(2, 0, 50, 0))),
+                    style=Pack(
+                        text_align="center",
+                        font_size="10",
+                        font_family="monospace",
+                        font_weight="bold",
+                        color=rgb(0, 179, 241),
+                        padding=(2, 0, 50, 0))),
             ],
             style=Pack(
                 direction=COLUMN,
@@ -70,7 +73,8 @@ class MainBalanceBox(toga.Box):
                     f"[ Blockchain Info ]",
                     style=Pack(
                         text_align="center",
-                        font_size="10",
+                        font_size="11",
+                        font_weight="bold",
                         font_family="monospace",
                         color=rgb(240, 248, 255))),
                 
@@ -84,47 +88,290 @@ class MainBalanceBox(toga.Box):
                 padding=5)
         )
         
-        blocks, header, difficulty, disk_size, commitments = self.get_blockchain_info()
+        blocks, header, difficulty, disk_size, commitments = get_blockchain_info(config_path)
+        
+        self.blocks_label = toga.Label(
+            f"Blocks :",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.blocks_label_box = toga.Box(
+            children=[
+                self.blocks_label
+            ],
+            style=Pack(
+                padding=5,
+                flex=1
+            )
+        )
+        
+        self.blocks_value = toga.Label(
+            f"{blocks}",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.blocks_value_box = toga.Box(
+            children=[
+                self.blocks_value
+            ],
+            style=Pack(
+                padding=5
+            )
+        )
+        
+        self.blocks_line = toga.Box(
+            children=[
+                self.blocks_label_box,
+                self.blocks_value_box
+            ],
+            style=Pack(
+                padding=(5, 20, 5, 20),
+                direction=ROW,
+                background_color=rgb(70, 70, 70)
+            )
+        )
+        
+        self.headers_label = toga.Label(
+            f"Headers :", 
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.headers_label_box = toga.Box(
+            children=[
+                self.headers_label
+            ],
+            style=Pack(
+                padding=5,
+                flex=1
+            )
+        )
+        
+        self.headers_value = toga.Label(
+            f"{header}", 
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.headers_value_box = toga.Box(
+            children=[
+                self.headers_value
+            ],
+            style=Pack(
+                padding=5
+            )
+        )
+        
+        self.headers_line = toga.Box(
+            children=[
+                self.headers_label_box,
+                self.headers_value_box
+            ],
+            style=Pack(
+                padding=(5, 20, 5, 20),
+                direction=ROW,
+                background_color=rgb(70, 70, 70)
+            )
+        )
+        
+        self.difficulty_label = toga.Label(
+            f"Difficulty :",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.difficulty_label_box = toga.Box(
+            children=[
+                self.difficulty_label
+            ],
+            style=Pack(
+                padding=5,
+                flex=1
+            )
+        )
+        
+        self.difficulty_value = toga.Label(
+            f"{float(difficulty):.2f}",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.difficulty_value_box = toga.Box(
+            children=[
+                self.difficulty_value
+            ],
+            style=Pack(
+                padding=5
+            )
+        )
+        
+        self.difficulty_line = toga.Box(
+            children=[
+                self.difficulty_label_box,
+                self.difficulty_value_box
+            ],
+            style=Pack(
+                padding=(5, 20, 5, 20),
+                direction=ROW,
+                background_color=rgb(70, 70, 70)
+            )
+        )
+        
+        self.commitments_label = toga.Label(
+            f"Commitments :",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.commitments_label_box = toga.Box(
+            children=[
+                self.commitments_label
+            ],
+            style=Pack(
+                padding=5,
+                flex=1
+            )
+        )
+        
+        self.commitments_value = toga.Label(
+            f"{commitments}",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.commitments_value_box = toga.Box(
+            children=[
+                self.commitments_value
+            ],
+            style=Pack(
+                padding=5
+            )
+        )
+        
+        self.commitments_line = toga.Box(
+            children=[
+                self.commitments_label_box,
+                self.commitments_value_box
+            ],
+            style=Pack(
+                padding=(5, 20, 5, 20),
+                direction=ROW,
+                background_color=rgb(70, 70, 70)
+            )
+        )
+        
+        self.sizeondisk_label = toga.Label(
+            f"Size On Disk :",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.sizeondisk_label_box = toga.Box(
+            children=[
+                self.sizeondisk_label
+            ],
+            style=Pack(
+                padding=5,
+                flex=1
+            )
+        )
+        
+        self.sizeondisk_value = toga.Label(
+            f"{float(disk_size):.1f} GB",
+            style=Pack(
+                font_size="10",
+                font_family="monospace",
+                font_weight="bold",
+                text_align="center",
+                color=rgb(240, 248, 255),
+                padding=5
+            )
+        )
+        
+        self.sizeondisk_value_box = toga.Box(
+            children=[
+                self.sizeondisk_value
+            ],
+            style=Pack(
+                padding=5
+            )
+        )
+        
+        self.sizeondisk_line = toga.Box(
+            children=[
+                self.sizeondisk_label_box,
+                self.sizeondisk_value_box
+            ],
+            style=Pack(
+                padding=(5, 20, 5, 20),
+                direction=ROW,
+                background_color=rgb(70, 70, 70)
+            )
+        )
         
         self.blockchain_info = toga.Box(
             children=[
-                toga.Label(
-                    f"Blocks : {blocks}",
-                    style=Pack(font_size="10",
-                    font_family="monospace",
-                    color=rgb(240, 248, 255),
-                    padding=(10, 0, 5, 10))),
-                
-                toga.Label(
-                    f"Headers : {header}", 
-                    style=Pack(font_size="10",
-                    font_family="monospace",
-                    color=rgb(240, 248, 255),
-                    padding=(0, 0, 5, 10))),
-                
-                toga.Label(
-                    f"Difficulty : {float(difficulty):.2f}",
-                    style=Pack(font_size="10",
-                    font_family="monospace",
-                    color=rgb(240, 248, 255),
-                    padding=(0, 0, 5, 10))),
-                
-                toga.Label(
-                    f"Commitments : {commitments}",
-                    style=Pack(font_size="10",
-                    font_family="monospace",
-                    color=rgb(240, 248, 255),
-                    padding=(0, 0, 5, 10))),
-                
-                toga.Label(
-                    f"Size On Disk : {float(disk_size):.1f} GB",
-                    style=Pack(
-                        font_size="10",
-                        font_family="monospace",
-                        color=rgb(240, 248, 255),
-                        padding=(0, 0, 10, 10)
-                        )
-                    ),
+                self.blocks_line,
+                self.headers_line,
+                self.difficulty_line,
+                self.commitments_line,
+                self.sizeondisk_line
             ],
             style=Pack(
                 direction=COLUMN,
@@ -138,105 +385,3 @@ class MainBalanceBox(toga.Box):
             self.balances_box_devider,
             self.blockchain_info
         )
-
-    def format_total_balance(self, total):
-        formatted_total = '{:.8f}'.format(total)  
-        parts = formatted_total.split('.')  
-        integer_part = parts[0]
-        decimal_part = parts[1] 
-
-        if len(integer_part) > 4:
-            digits_to_remove = len(integer_part) - 4
-            formatted_decimal = decimal_part[:-digits_to_remove]
-        else:
-            formatted_decimal = decimal_part
-
-        formatted_balance = integer_part + '.' + formatted_decimal
-        return formatted_balance
-
-    def get_total_balance(self):
-        config_path = self.app.paths.config / 'config.json'
-
-        if os.path.exists(config_path):
-            with open(config_path, "r") as config_file:
-                config = json.load(config_file)
-                rpc_user = config.get("rpcuser")
-                rpc_password = config.get("rpcpassword")
-                rpc_host = config.get("rpchost")
-                rpc_port = config.get("rpcport")
-                url = f"http://{rpc_host}:{rpc_port}"
-
-                headers = {"content-type": "text/plain"}
-                payload = {
-                    "jsonrpc": "1.0",
-                    "id": "curltest",
-                    "method": "z_gettotalbalance",
-                    "params": [],
-                }
-                response = requests.post(
-                    url,
-                    data=json.dumps(payload),
-                    headers=headers,
-                    auth=(rpc_user, rpc_password),
-                )
-
-                if response.status_code == 200:
-                    data = response.json()
-                    data_result = data["result"]
-                    total = self.format_total_balance(
-                        float(data_result["total"])
-                    )
-                    transparent = self.format_total_balance(
-                        float(data_result["transparent"])
-                    )
-                    private = self.format_total_balance(
-                        float(data_result["private"])
-                    )
-        else:
-            total = "0.00"
-            transparent = "0.00"
-            private = "0.00"
-        return total, transparent, private
-        
-        
-    def get_blockchain_info(self):
-        config_path = self.app.paths.config / 'config.json'
-        if os.path.exists(config_path):
-            with open(config_path, "r") as config_file:
-                config = json.load(config_file)
-                rpc_user = config.get("rpcuser")
-                rpc_password = config.get("rpcpassword")
-                rpc_host = config.get("rpchost")
-                rpc_port = config.get("rpcport")
-                url = f"http://{rpc_host}:{rpc_port}"
-
-                headers = {"content-type": "text/plain"}
-                payload = {
-                    "jsonrpc": "1.0",
-                    "id": "curltest",
-                    "method": "getblockchaininfo",
-                    "params": [],
-                }
-                response = requests.post(
-                    url,
-                    data=json.dumps(payload),
-                    headers=headers,
-                    auth=(rpc_user, rpc_password),
-                )
-
-                if response.status_code == 200:
-                    data = response.json()
-                    data_result = data["result"]
-                    blocks = data_result["blocks"]
-                    header = data_result["headers"]
-                    difficulty = data_result["difficulty"]
-                    disk_size = data_result["size_on_disk"]
-                    disk_size_gb = disk_size / (1024**3)
-                    commitments = data_result["commitments"]
-        else:
-            blocks = "0"
-            header = "0"
-            difficulty = "0"
-            disk_size_gb = "0"
-            commitments = "0"
-        return blocks, header, difficulty, disk_size_gb, commitments
